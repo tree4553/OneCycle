9e1936017d45b4f36089ee3d861b0804be9c9987
Jeong-cheol Lee <tree4553@naver.com>
2022-05-11 22:16:57 +0900

    3.4 등록/수정/조회 API 만들기

--- src/main/java/com/onecycle/web/PostsApiController.java
+++ src/main/java/com/onecycle/web/PostsApiController.java
@@ -1,4 +1,30 @@
 package com.onecycle.web;
 
+import com.onecycle.service.posts.PostsService;
+import com.onecycle.web.dto.PostsResponseDto;
+import com.onecycle.web.dto.PostsSaveRequestDto;
+import com.onecycle.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
 public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
 }
