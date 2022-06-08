c668441ef5b8c0a394aaf4b46dcd4e777459a8c8
Jeong-cheol Lee <tree4553@naver.com>
2022-06-08 17:04:34 +0900

    4.3 게시글 등록 화면 만들기 4.4 전체 조회 화면 만들기

--- src/main/java/com/onecycle/service/posts/PostsService.java
+++ src/main/java/com/onecycle/service/posts/PostsService.java
@@ -2,13 +2,17 @@ package com.onecycle.service.posts;
 
 import com.onecycle.domain.posts.Posts;
 import com.onecycle.domain.posts.PostsRepository;
+import com.onecycle.web.dto.PostsListResponseDto;
 import com.onecycle.web.dto.PostsResponseDto;
 import com.onecycle.web.dto.PostsSaveRequestDto;
 import com.onecycle.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+//import javax.transaction.Transactional; // 다른 라이브러리 import로 @Transaction(readOnly=true)에 문제가 발생했었다.
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +35,10 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
